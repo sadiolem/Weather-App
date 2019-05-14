fb037e49b50918f1f4e071fcd7b130326794b8aa
sadiolem <sadiolem.sds@gmail.com>
2019-05-15 00:06:38 +0300

    change code style

--- public/script.js
+++ public/script.js
@@ -1,56 +1,58 @@
-const locationEl = document.querySelector('[data-location]'),
-      timeEl = document.querySelector('[data-time]'),
-      temperatureEl = document.querySelector('[data-temperature]'),
-      precipitationEl = document.querySelector('[data-precipitation]'),
-      humidityEl = document.querySelector('[data-humidity]'),
-      windEl = document.querySelector('[data-wind]'),
-      searchElement = document.querySelector('[data-city-search]')
-
-const searchBox = new google.maps.places.SearchBox(searchElement)
+const searchElement = document.querySelector('[data-city-search]');
+const searchBox = new google.maps.places.SearchBox(searchElement);
 
 searchBox.addListener('places_changed', () => {
-  const place = searchBox.getPlaces()[0]
-  if (place == null) return
-  const latitude = place.geometry.location.lat()
-  const longitude = place.geometry.location.lng()
+  const place = searchBox.getPlaces()[0];
+  const latitude = place.geometry.location.lat();
+  const longitude = place.geometry.location.lng();
+
+  if (place == null) return;
 
   fetch('/weather', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    body: JSON.stringify({
-      latitude,
-      longitude
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        latitude,
+        longitude
+      })
     })
-  })
-  .then(res => res.json())
-  .then(data => {setWeather(data, place.formatted_address)})
-})
+    .then(res => res.json())
+    .then(data => {
+      setWeather(data, place.formatted_address)
+    });
+});
 
+const locationEl = document.querySelector('[data-location]');
+const timeEl = document.querySelector('[data-time]');
+const temperatureEl = document.querySelector('[data-temperature]');
+const precipitationEl = document.querySelector('[data-precipitation]');
+const humidityEl = document.querySelector('[data-humidity]');
+const windEl = document.querySelector('[data-wind]');
 const icon = new Skycons({
   color: '#263238'
-})
+});
 
-icon.set('icon', 'clear-day')
-icon.play()
+icon.set('icon', 'clear-day');
+icon.play();
 
 function setWeather(data, place) {
-  const date = new Date(data.time * 1000),
-        day = date.getDay(),
-        weekDays = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
-        hours = date.getHours(),
-        minutes = `0${date.getMinutes()}`,
-        formattedTime = `${weekDays[day]} ${hours}:${minutes.substr(-2)}`
+  const date = new Date(data.time * 1000);
+  const day = date.getDay();
+  const weekDays = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
+  const hours = date.getHours();
+  const minutes = `0${date.getMinutes()}`;
+  const formattedTime = `${weekDays[day]} ${hours}:${minutes.substr(-2)}`;
 
-  locationEl.textContent = place
-  timeEl.textContent = formattedTime
-  temperatureEl.innerHTML = data.temperature + '<span class="cels">°C</span>'
-  precipitationEl.textContent = `${data.precipProbability * 100}%`
-  humidityEl.textContent = `${data.humidity * 100}%`
-  windEl.textContent = data.windSpeed + ' м/с'
+  locationEl.textContent = place;
+  timeEl.textContent = formattedTime;
+  temperatureEl.innerHTML = data.temperature + '<span class="cels">°C</span>';
+  precipitationEl.textContent = `${data.precipProbability * 100}%`;
+  humidityEl.textContent = `${data.humidity * 100}%`;
+  windEl.textContent = data.windSpeed + ' м/с';
 
-  icon.set('icon', data.icon)
-  icon.play()
+  icon.set('icon', data.icon);
+  icon.play();
 }
--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+  require('dotenv').config();
 }
 
-const DarkSkyAPI_KEY = process.env.DarkSkyAPI_KEY
-const axios = require('axios')
-const express = require('express')
-const app = express()
+const DarkSkyAPI_KEY = process.env.DarkSkyAPI_KEY;
+const axios = require('axios');
+const express = require('express');
+const app = express();
 
-app.use(express.json())
-app.use(express.static('public'))
+app.use(express.json());
+app.use(express.static('public'));
 
 app.post('/weather', (req, res) => {
   const url = `https://api.darksky.net/forecast/5b78c2e40f30cbf8ffdab9dd65e23dbe/${req.body.latitude},${req.body.longitude}?units=si`
   axios({
-    url: url,
-    responseType: 'json'
-  })
-  .then(data => res.json(data.data.currently))
-})
+      url: url,
+      responseType: 'json'
+    })
+    .then(data => res.json(data.data.currently))
+});
 
 app.listen(3000, () => {
-  console.log('Server Started')
-})
+  console.log('Server Started');
+});
