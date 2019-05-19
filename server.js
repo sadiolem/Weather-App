d476fbb0c0b247143279f579ffbea050eb0ac16a
sadiolem <sadiolem.sds@gmail.com>
2019-05-19 10:50:01 +0300

    add forecast boxes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.post('/weather', (req, res) => {
       url: url,
       responseType: 'json'
     })
-    .then(data => res.json(data.data.currently));
+    .then(data => res.json(data.data.daily));
 });
 
 app.listen(3000, () => {
